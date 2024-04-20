cd72d5459bcf397ae2e1751b9a4bf57d42637aaa
MeGallin <me@garyallin.uk>
2024-04-20 14:51:15 +0100

    basic setup reset

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import Header from './common/Header'
+import Footer from './common/Footer'
+
+function App() {
+ 
+
+  return (
+    <>
+  <Header/>
+  <main/>
+  <Footer/>
+    </>
+  )
+}
+
+export default App
