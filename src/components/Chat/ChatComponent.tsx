d320287f2d98d15a120e6ae86c8201c1d7ec3e32
MeGallin <me@garyallin.uk>
2024-05-02 15:06:44 +0100

    Design Tidy up

--- src/components/Chat/ChatComponent.tsx
+++ src/components/Chat/ChatComponent.tsx
@@ -1,49 +1,52 @@
 import { BubbleChat } from 'flowise-embed-react';
+import './ChatComponent.css';
 const ChatComponent = () => {
   return (
-    <BubbleChat
-      chatflowid="4cf8df7e-09a3-45f5-bd37-8b5791d6b836"
-      apiHost="https://flowise-ai-cls.onrender.com"
-      theme={{
-        button: {
-          backgroundColor: 'rgba(59, 129, 246, 1)',
-          right: 6,
-          bottom: 48,
-          size: 'large',
-          iconColor: 'white',
-          customIconSrc:
-            'https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg',
-        },
-        chatWindow: {
-          welcomeMessage: 'Hi, I am here to help you with CLS.',
-          backgroundColor: '#ffffff',
-          height: 650,
-          width: 400,
-          fontSize: 16,
-          poweredByTextColor: '#ffffff',
-          botMessage: {
-            backgroundColor: '#f7f8ff',
-            textColor: '#303235',
-            showAvatar: false,
-            avatarSrc:
-              'https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/parroticon.png',
+    <div>
+      <BubbleChat
+        chatflowid="4cf8df7e-09a3-45f5-bd37-8b5791d6b836"
+        apiHost="https://flowise-ai-cls.onrender.com"
+        theme={{
+          button: {
+            backgroundColor: 'rgba(59, 129, 246, 1)',
+            right: 6,
+            bottom: 0,
+            size: 'medium',
+            iconColor: 'white',
+            customIconSrc:
+              'https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg',
           },
-          userMessage: {
-            backgroundColor: '#3B81F6',
-            textColor: '#ffffff',
-            showAvatar: false,
-            avatarSrc:
-              'https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png',
+          chatWindow: {
+            welcomeMessage: '',
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+            height: 650,
+            width: 400,
+            fontSize: 16,
+            poweredByTextColor: 'silver',
+            botMessage: {
+              backgroundColor: '#f7f8ff',
+              textColor: '#303235',
+              showAvatar: false,
+              avatarSrc:
+                'https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/parroticon.png',
+            },
+            userMessage: {
+              backgroundColor: 'rgba(12,12,12,1',
+              textColor: '#ffffff',
+              showAvatar: false,
+              avatarSrc:
+                'https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png',
+            },
+            textInput: {
+              placeholder: 'Type your or ask a question',
+              backgroundColor: '#ffffff',
+              textColor: '#303235',
+              sendButtonColor: '#3B81F6',
+            },
           },
-          textInput: {
-            placeholder: 'Type your or ask a question',
-            backgroundColor: '#ffffff',
-            textColor: '#303235',
-            sendButtonColor: '#3B81F6',
-          },
-        },
-      }}
-    />
+        }}
+      />
+    </div>
   );
 };
 
--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,12 +1,48 @@
 import ChatComponent from '../Chat/ChatComponent';
-
+import './HomeComponent.css';
 const HomeComponent = () => {
   return (
-    <>
-      <h1>HomeComponent</h1>
-      <p>This page will contain the button to ask questions</p>
+    <div className="home-wrapper">
+      <h1>Mercedes-Benz CLS Operator Manual Chatbot!</h1>
+      <p>
+        <em>Unlock the Power of Knowledge with Ease</em>
+      </p>
+      <h2>Introduction:</h2>
+      <p>
+        Welcome to the ultimate guide for navigating your Mercedes-Benz CLS.
+        Whether you're a seasoned driver or just getting acquainted with your
+        vehicle, our chatbot is here to make your journey smoother. Say goodbye
+        to flipping through thick manuals and hello to instant, personalized
+        assistance right at your fingertips.
+      </p>
+      <h2>What We Do:</h2>
+      <p>
+        Our chatbot provides quick and intuitive access to the wealth of
+        information found in your Mercedes-Benz CLS operator manual. From
+        understanding warning lights to mastering advanced features, our chatbot
+        is your virtual companion on the road. With simple conversational
+        prompts, you can effortlessly find answers to your questions, ensuring
+        that you get the most out of your driving experience.
+      </p>
+      <h2>How to Use:</h2>
+      <p>
+        Using our chatbot is as easy as starting your engine. Simply type your
+        question or concern into the chat window, and our intelligent system
+        will provide you with the relevant information in real-time. Whether
+        you're troubleshooting a problem or exploring new features, our chatbot
+        is always ready to assist you, 24/7.
+      </p>
+      <h2>Get Started:</h2>
+      <p>
+        Ready to unlock the full potential of your Mercedes-Benz CLS? Click
+        below to start chatting with our knowledgeable bot and take control of
+        your driving experience today.
+      </p>
+      <p>
+        <strong>Start Chatting Now</strong>
+      </p>
       <ChatComponent />
-    </>
+    </div>
   );
 };
 
